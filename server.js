b0635087976ad7844c2657a84749c147ee45b263
Sawyer Fisher <sawyerkove@gmail.com>
2020-03-14 21:38:52 -0500

    successfully connected to database

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+//Required Dependencies for functionality
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    PORT: 3306,
+    user: "root",
+    password: "Thatsmydawg69!",
+    database: "COMPANY_DB"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connection as id:" + connection.threadId);
+    console.log("worked dude")
+  });
